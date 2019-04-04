cf51a9d32761ecffbb053204b0c544e2b34b78d6
Noel Branzuela <noelbranz@gmail.com>
2019-04-04 18:13:21 +1300

    added hashHistory

---
+++ precache-manifest.c45bf4d37149bce4818a15bf66bd6cbb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "67f005e3685e2ca0f8bb",
+    "url": "/static/css/main.68d39e4b.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "d38995796cf3364af879",
+    "url": "/static/js/2.63eac8d4.chunk.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "67f005e3685e2ca0f8bb",
+    "url": "/static/js/main.08f37d1f.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "d38995796cf3364af879",
+    "url": "/static/css/2.ae84dfbf.chunk.css"
+  },
+  {
+    "revision": "0859906204090b3f1cb94a797a236313",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94a7ae91990412cfb51ec94e5164ce80.js"
+  "/precache-manifest.c45bf4d37149bce4818a15bf66bd6cbb.js"
 );
 
 workbox.clientsClaim();
